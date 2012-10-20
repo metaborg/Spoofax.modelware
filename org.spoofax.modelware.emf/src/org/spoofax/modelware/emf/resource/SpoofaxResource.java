b742af9fb8a7a0630e60053e6be8ea83d424c1d0
OskarVanRest <OskarVanRest@ba653a0a-7fb7-0310-a048-973b18a224e4>
2012-10-20 15:04:57 +0000

    doSave() backup... still bug somewhere

--- org.spoofax.modelware.emf/src/org/spoofax/modelware/emf/resource/SpoofaxResource.java
+++ org.spoofax.modelware.emf/src/org/spoofax/modelware/emf/resource/SpoofaxResource.java
@@ -1,5 +1,6 @@
 package org.spoofax.modelware.emf.resource;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,32 +15,46 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.impl.EPackageRegistryImpl;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
 import org.eclipse.imp.model.ModelFactory.ModelException;
+import org.spoofax.interpreter.terms.IStrategoString;
 import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.modelware.emf.Model2Term;
 import org.spoofax.modelware.emf.Term2Model;
+import org.spoofax.terms.TermFactory;
+import org.strategoxt.imp.generator.construct_textual_change_4_0;
 import org.strategoxt.imp.runtime.FileState;
 import org.strategoxt.imp.runtime.dynamicloading.BadDescriptorException;
+import org.strategoxt.imp.runtime.dynamicloading.Descriptor;
+import org.strategoxt.imp.runtime.dynamicloading.RefactoringFactory;
+import org.strategoxt.imp.runtime.services.StrategoObserver;
+import org.strategoxt.lang.Context;
+import org.strategoxt.lang.Strategy;
 
 public class SpoofaxResource extends ResourceImpl {
 
+	private IPath filePath;
+	private FileState fileState;
+
 	public SpoofaxResource(URI uri) {
 		this.uri = uri;
+
+		URI resolvedFile = CommonPlugin.resolve(uri);
+		this.filePath = new Path(resolvedFile.toFileString());
+
+		try {
+			fileState = FileState.getFile(filePath, null);
+		} catch (FileNotFoundException | BadDescriptorException | ModelException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @override
 	 */
 	protected void doLoad(InputStream inputStream, Map<?, ?> options) {
-		System.out.println("doLoad");
-		
-		URI resolvedFile = CommonPlugin.resolve(uri);
-		IPath filePath = new Path(resolvedFile.toFileString());
-
-		FileState fileState = null;
 		IStrategoTerm analysedAST = null;
 		try {
-			fileState = FileState.getFile(filePath, null);
 			analysedAST = fileState.getAnalyzedAst();
-		} catch (FileNotFoundException | BadDescriptorException | ModelException e) {
+		} catch (BadDescriptorException e) {
 			e.printStackTrace();
 		}
 
@@ -56,11 +71,44 @@ public class SpoofaxResource extends ResourceImpl {
 	}
 
 	protected void doSave(OutputStream outputStream, Map<?, ?> options) {
-		String result = "";
+		EObject object = getContents().get(0);
+		Model2Term model2term = new Model2Term(new TermFactory());
+		IStrategoTerm resultTuple = model2term.convert(object);
+
+		File file = filePath.toFile();
+		Descriptor descriptor = fileState.getDescriptor();
+		IStrategoTerm textreplace = null;
+		String result = null;
+
 		try {
-			outputStream.write(result.getBytes());
+			StrategoObserver observer = descriptor.createService(StrategoObserver.class, fileState.getParseController());
+			textreplace = construct_textual_change_4_0.instance.invoke(observer.getRuntime().getCompiledContext(), resultTuple, createStrategy(RefactoringFactory.getPPStrategy(descriptor), file, observer), createStrategy(RefactoringFactory.getParenthesizeStrategy(descriptor), file, observer), createStrategy(RefactoringFactory.getOverrideReconstructionStrategy(descriptor), file, observer), createStrategy(RefactoringFactory.getResugarStrategy(descriptor), file, observer));
+
+			if (textreplace != null) {
+				result = ((IStrategoString) textreplace.getSubterm(0).getSubterm(2)).stringValue();
+			}
+		} catch (BadDescriptorException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			if (result == null)
+				outputStream.write("".getBytes());
+			else
+				outputStream.write(result.getBytes());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private Strategy createStrategy(final String sname, final File file, final StrategoObserver observer) {
+		return new Strategy() {
+			@Override
+			public IStrategoTerm invoke(Context context, IStrategoTerm current) {
+				if (sname != null)
+					return observer.invokeSilent(sname, current, file);
+				return null;
+			}
+		};
 	}
 }
