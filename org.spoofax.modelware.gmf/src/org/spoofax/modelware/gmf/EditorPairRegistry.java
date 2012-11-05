67c2a2517261fd6d5f7aac9baab5285d19a9090b
OskarVanRest <OskarVanRest@ba653a0a-7fb7-0310-a048-973b18a224e4>
2012-11-05 04:57:45 +0000

    Bugfix

--- org.spoofax.modelware.gmf/src/org/spoofax/modelware/gmf/EditorPairRegistry.java
+++ org.spoofax.modelware.gmf/src/org/spoofax/modelware/gmf/EditorPairRegistry.java
@@ -109,6 +109,9 @@ public class EditorPairRegistry {
 				String otherExtension;
 
 				Language language = LanguageRegistry.getInstance().get(extension);
+				if (language == null) {
+					return;
+				}
 
 				UniversalEditor textEditor = null;
 				DiagramEditor diagramEditor = null;
